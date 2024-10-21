028b45850d735cdec71f0cacaa93023791df7143
cpmoore0317 <cpmoore317@gmail.com>
2024-10-21 14:21:06 -0500

    Created register function in authprovider

--- components/AuthProvider.tsx
+++ components/AuthProvider.tsx
@@ -1,8 +1,13 @@
+import { auth } from "@/firebaseConfig";
+import { createUserWithEmailAndPassword } from "firebase/auth";
 import { createContext, ReactNode } from "react"
-
 
 const AuthContext = createContext({});
 
+function register(email: string, password: string) {
+    createUserWithEmailAndPassword(auth, email, password)
+}
+
 export function AuthProvider({ children }: { children: ReactNode }) {
-    return <AuthContext.Provider value={{}}>{children}</AuthContext.Provider>
+    return <AuthContext.Provider value={{ register }}>{children}</AuthContext.Provider>
 }
