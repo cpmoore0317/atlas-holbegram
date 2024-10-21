d1679fb378083bdc34b1d613b82614e763d4f08e
cpmoore0317 <cpmoore317@gmail.com>
2024-10-21 13:58:28 -0500

    Added firebase config

---
+++ firebaseConfig.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBf6WoHK-dHnNmOQ6ak_6F8mT2pkidPxBU",
+  authDomain: "holbegram-47eca.firebaseapp.com",
+  projectId: "holbegram-47eca",
+  storageBucket: "holbegram-47eca.appspot.com",
+  messagingSenderId: "350516550277",
+  appId: "1:350516550277:web:c34089ba356b61e4ece7ec"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
