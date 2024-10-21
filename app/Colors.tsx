7df1add4cb8c701720c2c0b2f51c16f77ec44814
cpmoore0317 <cpmoore317@gmail.com>
2024-10-20 20:20:18 -0500

    Testing styling

---
+++ app/Colors.tsx
@@ -0,0 +1,7 @@
+export const Colors = {
+    blue: "#00003C",
+    blueLight: "#64648C",
+    teal: "#1ED2AF",
+    yellow: "#edc14b",
+  };
+  
--- app/login.tsx
+++ app/login.tsx
@@ -1,10 +1,14 @@
 import { Link, useRouter } from "expo-router";
-import { Text, View, Pressable } from "react-native";
+import { Text, View, Pressable, Image, } from "react-native";
 
+import { Colors } from "./Colors";
 export default function Page() {
   const router = useRouter();
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center", backgroundColor: Colors.blue }}>
+      <Image
+          source={require('@/assets/images/atlas-logo.png')}
+        />
       <Text>Login</Text>
       <Link href="/register" replace>
         <Text>Create a new account</Text>
