b5b7ddd90ce12aaf6d75b00dc2a09b5cb628cb22
cpmoore0317 <cpmoore317@gmail.com>
2024-10-22 20:52:40 -0500

    Style change

--- app/(tabs)/add-post.tsx
+++ app/(tabs)/add-post.tsx
@@ -1,7 +1,19 @@
-import { Text, View } from "react-native";
+import { Pressable, Text, View } from "react-native";
+import { Colors } from "../Colors";
 
 export default function Page() {
     return <View style={{ flex: 1, alignItems: "center", justifyContent: "center", backgroundColor: "#F2F2F2"}}>
-        <Text>Add Post</Text>
+        <Pressable
+        style={{
+          width: "80%",
+          padding: 10,
+          backgroundColor: Colors.teal,
+          borderRadius: 5,
+          alignItems: "center",
+          marginTop: 10,
+        }}
+      >
+        <Text style={{ color: "white", fontSize: 16 }}>Upload Photo</Text>
+      </Pressable>
     </View>
 }
