e1b7e3ea167c62416ccb89a377371e641c8209d7
cpmoore0317 <cpmoore317@gmail.com>
2024-10-23 12:22:13 -0500

    Added placeholder image on upload image page

--- app/(tabs)/add-post.tsx
+++ app/(tabs)/add-post.tsx
@@ -1,19 +1,32 @@
-import { Pressable, Text, View } from "react-native";
+import { Pressable, Text, View, Image } from "react-native";
 import { Colors } from "../Colors";
 
 export default function Page() {
-    return <View style={{ flex: 1, alignItems: "center", justifyContent: "center", backgroundColor: "#F2F2F2"}}>
-        <Pressable
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "#F2F2F2",
+      }}
+    >
+      <Image
+        source={require("@/assets/images/cover-art.png")}
+        style={{ width: 300, height: 300, resizeMode: "contain", borderRadius: 7 }}
+      />
+      <Pressable
         style={{
           width: "80%",
-          padding: 10,
+          padding: 14,
           backgroundColor: Colors.teal,
-          borderRadius: 5,
+          borderRadius: 7,
           alignItems: "center",
           marginTop: 10,
         }}
       >
-        <Text style={{ color: "white", fontSize: 16 }}>Upload Photo</Text>
+        <Text style={{ color: "white", fontSize: 16 }}>Choose a photo</Text>
       </Pressable>
     </View>
+  );
 }
