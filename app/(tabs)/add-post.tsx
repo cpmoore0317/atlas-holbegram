2e2c3ae06125e10d44a53167a25e37783414ae04
cpmoore0317 <cpmoore317@gmail.com>
2024-10-22 20:38:46 -0500

    Updated colors

--- app/(tabs)/add-post.tsx
+++ app/(tabs)/add-post.tsx
@@ -1,7 +1,7 @@
 import { Text, View } from "react-native";
 
 export default function Page() {
-    return <View style={{ flex: 1, alignItems: "center", justifyContent: "center"}}>
+    return <View style={{ flex: 1, alignItems: "center", justifyContent: "center", backgroundColor: "#F2F2F2"}}>
         <Text>Add Post</Text>
     </View>
 }
