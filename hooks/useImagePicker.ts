1f92c5e55bc360112557a4db78cc4506fd2ef0c7
cpmoore0317 <cpmoore317@gmail.com>
2024-10-23 19:56:30 -0500

    Implemented image picker

--- hooks/useImagePicker.ts
+++ hooks/useImagePicker.ts
@@ -1,12 +1,28 @@
-import { usePermissions } from './../node_modules/expo-media-library';
+import { usePermissions } from "expo-media-library";
 import { useState } from "react";
+import * as ImagePicker from "expo-image-picker"
 
 export function useImagePicker(){
-    const [ image, setImage ] = useState(undefined);
-    const {status, requestPermission} = usePermissions();
+    const [ image, setImage ] = useState<string | undefined>(undefined);
+    const [status, requestPermission] = usePermissions();
 
-    function openImagePicker(){
-        alert("Open Image Picker");
+    async function openImagePicker(){
+        if(status === null){
+            const permission = await requestPermission();
+            if(permission.granted === false){
+                alert("You need to grant permission to acces your images");
+                return;
+            }
+        }
+
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.Images,
+            allowsEditing: true,
+        });
+
+        if(!result.canceled) {
+            setImage(result.assets[0].uri);
+        }
     }
 
     function reset(){
