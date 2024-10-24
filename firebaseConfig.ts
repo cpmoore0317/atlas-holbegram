fe13526a6ebb343449c6746c78b1668ec3488857
cpmoore0317 <cpmoore317@gmail.com>
2024-10-23 20:19:40 -0500

    Upload method

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { initializeAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -16,5 +17,9 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
 // Initialise Auth
 export const auth = initializeAuth(app);
+
+// Initialize Storage
+export const storage = getStorage(app);
